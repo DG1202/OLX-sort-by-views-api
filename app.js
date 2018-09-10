837c4388871d47cc399355a8d177a02e93e6acfc
MishaHnatyshyn <gnatyshynmisha@gmail.com>
2018-09-10 22:45:47 +0300

    v1.0

--- app.js
+++ app.js
@@ -1,28 +1,68 @@
 'use strict';
-
 const express = require('express');
 const request = require('request-promise');
 const app = express();
 
 const options = {
-    method: 'GET',
-    uri: 'https://www.olx.ua/obyavlenie/so-dimm-ddr3l-ddr3-4gb-1600-mhz-samsung-adata-elixir-IDApFh6.html?sd=1#627eb08d0e',
-    headers:{
-        'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.181 Safari/537.36',
-        referer: 'https://www.olx.ua/',
-        'upgrade-insecure-requests': 1,
-        'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
-        'accept-language': 'en-US,en;q=0.9,uk;q=0.8,ru;q=0.7',
-        'cache-control': 'max-age=0',
-    }
+  method: 'GET',
+  headers:{
+    'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.181 Safari/537.36',
+    referer: 'https://www.olx.ua/',
+    'upgrade-insecure-requests': 1,
+    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+    'accept-language': 'en-US,en;q=0.9,uk;q=0.8,ru;q=0.7',
+    'cache-control': 'max-age=0',
+  }
 };
 
-request(options).then(data=>{console.log(data.indexOf('Просмотры'))}).catch();
+const getViewsCount = text => {
+  const start = text.indexOf('<div class="pdingtop10">')+ 2;
+  return text.substring(text.indexOf(('<strong>') , start) + 8 , text.indexOf('</strong>', start ));
+};
+
+const parse = text => {
+  const result = [];
+  const word = '<h3 class="lheight22 margintop5">';
+  let pos = text.indexOf(word);
+
+  while(pos > 0){
+    const url = text.substring(text.indexOf('href="',pos) +6  ,text.indexOf('" class',pos) );
+    const name = text.substring(text.indexOf('g>',pos) +2 , text.indexOf('</strong>',pos ));
+    result.push( {url,name} );
+    pos = text.indexOf(word, pos + 1);
+  }
+  return result
+};
 
 app.get(/api/,(req, res)=>{
-    const uri = req.query.uri;
-    // res.end()
+  const PromiseItemsList = [];
+  for (let i = 1; i < req.query.pages; i++){
+    const uri = req.query.uri+'?page='+i;
+    console.log(uri);
+    PromiseItemsList.push(request(Object.assign({uri:uri}, options)).then(data=>parse(data)))
+  }
+
+  Promise.all(PromiseItemsList).then(itemArrays => {
+    let result = [];
+
+    for (let i = 0; i < 2; i++){
+      result = result.concat(itemArrays[i])
+    }
+    console.log(result);
+    const PromiseList = [];
+    result.forEach(item=>{
+      PromiseList.push(request(Object.assign({uri:item.url}, options)))
+    });
+
+    Promise.all(PromiseList).then(viewsCount => {
+      console.log('VIEWS COUNT')
+      for (let i = 0;i < viewsCount.length;i++){
+        result[i].views = getViewsCount(viewsCount[i])
+      }
+      result.sort((a,b)=>b.views - a.views);
+      res.json(result)
+    }).catch()
+  }).catch();
 });
 
 app.listen(8080);
-
