c31b924ebec9ebeb5487fbce613d627b6a45d3f1
Dmytro Gnatyshyn <di1202@ukr.net>
2018-09-21 12:13:15 +0300

    add .

--- list.js
+++ list.js
@@ -30,4 +30,4 @@ function displayResult() {
     xhttp.open("GET", "/api?uri= " + searchValue + "&pages=" + pagesValue, true);
     xmlhttp.send();
 }
-document.getElementById("searchButtom").onclick =displayResult();
+document.getElementById("searchButtom").onclick = displayResult();
